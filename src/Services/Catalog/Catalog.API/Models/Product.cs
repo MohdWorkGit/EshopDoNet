ca9ea29a853d48f313635a4725a720a24e952c95
mohdworkgit <mohdworkmail@gmail.com>
2025-01-15 13:50:42 +0400

    catalog api

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public List<string> Catagory { get; set; } = new();
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+   
+
+{
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, String Imagefile, decimal Price) 
+        : ICommand<CreateProductResult> ;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Business logic to create product
+            var product = new Product
+            {
+                Name = command.Name,
+                Catagory = command.Category,
+                ImageFile = command.Imagefile,
+                Price = command.Price,
+                Description = command.Description
+            };
+
+            //save to DB
+
+            return new CreateProductResult(Guid.NewGuid());
+
+        }
+    }
+}
